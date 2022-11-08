a1940bbc0e08563377d17c20e968331c49314dc0
PGZeeD <ezeadh2690@gmail.com>
2022-11-08 12:55:27 -0300

    avances

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,19 @@
-import { validationResult } from 'express-validator';
+import { User } from '../models/User.js';
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
   res.json({ ok: 'Register' });
+
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    //jwt token
+
+    return res.json({ ok: true });
+  } catch (error) {
+    console.log(error);
+  }
 };
 export const login = (req, res) => {
   res.json({ ok: 'Login' });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcryptjs from 'bcryptjs';
 
 const { Schema, model } = mongoose;
 
@@ -17,4 +18,17 @@ const userSchema = new Schema({
   },
 });
 
-export const User = model('user', userSchema);
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    user.password = bcryptjs.hash(user.password, salt);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new Error('Fallo el hash de contraseña');
+  }
+});
+
+export const User = mongoose.model('User', userSchema);
