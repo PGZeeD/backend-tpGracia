633bc0eb068245f44d3d76cf34f7a59047b0361c
PGZeeD <ezeadh2690@gmail.com>
2022-11-08 12:15:52 -0300

    ❤❤❤

---
+++ middlewares/validationResultExpress.js
@@ -0,0 +1,10 @@
+import { validationResult } from 'express-validator';
+
+export const validationResultExpress = (req, res, next) => {
+  const errors = validationResult(req);
+  console.log(req.body);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
